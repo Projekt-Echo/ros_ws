170cd60eb6f5f22dc2e3bc91c805342375ba934d
Nymphaea <mail@supaku.cn>
2025-03-21 23:52:02 +0800

    [refactor] Delete the algo which calc distancn by fitting line
    Signed-off-by: Nymphaea <mail@supaku.cn>

--- src/position/setup.py
+++ src/position/setup.py
@@ -20,8 +20,7 @@ setup(
 	tests_require=['pytest'],
 	entry_points={
 		'console_scripts': [
-			"direct = position.direct:main",
-			"fitting = position.fitting:main",
+			"main = position.direct:main",
 		],
 	},
 )
