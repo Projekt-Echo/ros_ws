4c4a630b7861ab6bd6c73385cb3525f905652939
Nymphaea <mail@supaku.cn>
2025-03-21 19:42:30 +0800

    [refactor] Refactored the driver of JY61P IMU
    Signed-off-by: Nymphaea <mail@supaku.cn>

--- src/imu/imu/main.py
+++ src/imu/imu/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+import struct
+
 import rclpy
 import smbus
+from geometry_msgs.msg import Vector3
 from rclpy.node import Node
 from sensor_msgs.msg import Imu
 
@@ -11,36 +14,19 @@ class JY61PDriver(Node):
 		self.bus = smbus.SMBus(1)  # 使用I2C总线1
 		self.address = 0x50  # JY61P的I2C地址
 		self.imu_pub = self.create_publisher(Imu, 'imu/data', 10)
+		self.angles_pub = self.create_publisher(Vector3, 'imu/angles', 10)
 		self.timer = self.create_timer(0.1, self.timer_callback)  # 每0.1秒读取一次数据
-		self.declare_parameter('frame_id', 'jy61p_link')  # 声明参数，用于设置IMU数据的参考系
+		self.declare_parameter('frame_id', 'imu_link')  # 声明参数，用于设置IMU数据的参考系
 		self.frame_id = self.get_parameter('frame_id').value
 
 	def timer_callback(self):
 		try:
-			# 读取加速度数据
-			ax = self.read_word_2c(0x34)
-			ay = self.read_word_2c(0x35)
-			az = self.read_word_2c(0x36)
-
-			# 读取角速度数据
-			gx = self.read_word_2c(0x37)
-			gy = self.read_word_2c(0x38)
-			gz = self.read_word_2c(0x39)
-
-			# 读取四元数数据
+			# 读取传感器数据
+			roll, pitch, yaw = self.read_angles()
+			ax, ay, az = self.read_accel()
+			gx, gy, gz = self.read_velocity()
 			quaternion = self.read_quaternion()
 
-			# 读取温度数据
-			temperature = self.read_word_2c(0x40)
-
-			# 转换为物理量
-			ax = ax / 32768.0 * 16 * 9.8  # 转换为m/s²
-			ay = ay / 32768.0 * 16 * 9.8  # 转换为m/s²
-			az = az / 32768.0 * 16 * 9.8  # 转换为m/s²
-			gx = gx / 32768.0 * 2000  # 转换为°/s
-			gy = gy / 32768.0 * 2000  # 转换为°/s
-			gz = gz / 32768.0 * 2000  # 转换为°/s
-			temperature = temperature / 100.0  # 转换为摄氏度
 
 			# 创建并发布IMU消息
 			imu_msg = Imu()
@@ -57,36 +43,74 @@ class JY61PDriver(Node):
 			imu_msg.orientation.z = quaternion[2]
 			imu_msg.orientation.w = quaternion[3]
 
+			angles_msg = Vector3()
+			angles_msg.x = roll
+			angles_msg.y = pitch
+			angles_msg.z = yaw
+
 			self.imu_pub.publish(imu_msg)
+			self.angles_pub.publish(angles_msg)
 
 		except Exception as e:
 			self.get_logger().error('Error reading sensor data: %s' % str(e))
 
-	def read_word_2c(self, addr):
-		# 读取两个字节并转换为有符号的短整型
-		high = self.bus.read_byte_data(self.address, addr)
-		low = self.bus.read_byte_data(self.address, addr + 1)
-		val = (high << 8) + low
-		if val >= 0x8000:
-			return -((65535 - val) + 1)
-		else:
-			return val
+	def read_angles(self):
+		"""
+		从JY61P传感器读取角度
+		:return: 角度 (roll, pitch, yaw)
+		"""
+		angles_data = self.bus.read_i2c_block_data(self.address, 0x3D, 6)
+		val = struct.unpack("hhh", bytearray(angles_data))
+
+		roll = val[0] / 32768.0 * 180
+		pitch = val[1] / 32768.0 * 180
+		yaw = val[2] / 32768.0 * 180
+		return [roll, pitch, yaw]
+
+	def read_accel(self):
+		"""
+		从JY61P传感器读取加速度
+		:return: 加速度 (ax, ay, az)
+		"""
+		accel_data = self.bus.read_i2c_block_data(self.address, 0x34, 6)
+		val = struct.unpack("hhh", bytearray(accel_data))
+
+		# 归一化处理
+		ax = val[0] / 32768.0 * 16 * 9.8
+		ay = val[1] / 32768.0 * 16 * 9.8
+		az = val[2] / 32768.0 * 16 * 9.8
+
+		return [ax, ay, az]
+
+	def read_velocity(self):
+		"""
+		从JY61P传感器读取角速度
+		:return: 角速度 (gx, gy, gz)
+		"""
+		velocity_data = self.bus.read_i2c_block_data(self.address, 0x37, 6)
+		val = struct.unpack("hhh", bytearray(velocity_data))
+
+		# 归一化处理
+		gx = val[0] / 32768.0 * 2000
+		gy = val[1] / 32768.0 * 2000
+		gz = val[2] / 32768.0 * 2000
+
+		return [gx, gy, gz]
+
 
 	def read_quaternion(self):
 		"""
 		从JY61P传感器读取四元数
 		:return: 四元数 (x, y, z, w)
 		"""
-		q0 = self.read_word_2c(0x51)  # 读取四元数 q0
-		q1 = self.read_word_2c(0x52)  # 读取四元数 q1
-		q2 = self.read_word_2c(0x53)  # 读取四元数 q2
-		q3 = self.read_word_2c(0x54)  # 读取四元数 q3
+		quaternion_data = self.bus.read_i2c_block_data(self.address, 0x51, 8)
+		val = struct.unpack("hhhh", bytearray(quaternion_data))
 
 		# 归一化处理
-		q0 = q0 / 32768.0
-		q1 = q1 / 32768.0
-		q2 = q2 / 32768.0
-		q3 = q3 / 32768.0
+		q0 = val[0] / 32768.0
+		q1 = val[1] / 32768.0
+		q2 = val[2] / 32768.0
+		q3 = val[3] / 32768.0
 
 		return [q1, q2, q3, q0]
 
